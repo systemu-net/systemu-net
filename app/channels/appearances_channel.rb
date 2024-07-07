8908ea49d6f2a69c05445b67d4ba3da45fd92dfc
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2024-07-06 22:56:12 -0400

    Fix action cable

--- app/channels/appearances_channel.rb
+++ app/channels/appearances_channel.rb
@@ -2,16 +2,14 @@ class AppearancesChannel < ApplicationCable::Channel
   def subscribed
     redis.set("user_#{current_user.id}_online", "1")
     stream_from("appearances_channel")
-    ActionCable.server.broadcast "appearances_channel",
-                                 user_id: current_user.id,
-                                 online: true
+    ActionCable.server.broadcast \
+      "appearances_channel", { user_id: current_user.id, online: true }
   end
 
   def unsubscribed
     redis.del("user_#{current_user.id}_online")
-    ActionCable.server.broadcast "appearances_channel",
-                                 user_id: current_user.id,
-                                 online: false
+    ActionCable.server.broadcast \
+      "appearances_channel", { user_id: current_user.id, online: false }
   end
 
   private
